8730acefe9f88a2eae809e8ad692e459dd5bb9e9
Ryan Beesley <rbeesley@users.noreply.github.com>
2021-04-02 15:17:47 -0700

    I was flattening the grays to grayscale Not exactly what I wanted, so I made adjustments. The different themes make this a difficult problem. I tried to make adjustments so that 40, 47, 100, and 107 held at the hue in the theme, but this couldn't always be achieved without holding another color. The tolerance set point is Tango Dark's 105. Unfortunately there are some themes where one of the term colors actually exceeds the saturation of one of the gray term colors. This isn't a perfect solution for that reason. Some themes just won't play well, but other than 40 and 100 on Solarized Dark, this seems to capture what I was trying to accomplish and it keeps the tonal colors of the grays unaltered in the theme provided they're within the tolerance.

--- hueshift.hlsl
+++ hueshift.hlsl
@@ -10,11 +10,22 @@ cbuffer PixelShaderSettings {
 	float4 Background;
 };
 
+// Set the CHANGE_RATE to 0.0f and fix the hue to a specific value with HUE_OFFSET
+// if you don't want the colors to shift over time but you want the colors to
+// be adjusted.
 #define HUE_OFFSET 0.0f
-// As close to 1/6 as possible with float
-#define CHANGE_RATE 0.16666667163372039794921875f
-// #define CHANGE_RATE 0.01f
-#define TOLERANCE 0.2f
+// As close to 1/6 as possible with float. For standard term color definitions
+// with 6 non-gray scale "primary" colors, this will cycle through the colors
+// once a second at each intensity before repeating.
+// #define CHANGE_RATE 0.16666667163372039794921875f
+#define CHANGE_RATE 0.01f
+// Use a tool like https://www.rapidtables.com/convert/color/rgb-to-hsv.html
+// to find the saturation of the color you want to be the set point if the color
+// isn't changing. The tolerance changes values >= this number, so if you see
+// the saturation for #AD7FA8 is 26.6%, 0.266f will be the saturation level from
+// which all saturations are compared. #AE9A06 for instance has a saturation of
+// 96.1% so it will meet this threshold.
+#define TOLERANCE 0.266f
 
 float3 rgb_to_hsv_no_clip(float3 RGB)
 {
@@ -35,7 +46,7 @@ float3 rgb_to_hsv_no_clip(float3 RGB)
     HSV.xy = 0;
     HSV.z = maxChannel;
     float delta = maxChannel - minChannel;             //Delta RGB value 
-    if (delta >= TOLERANCE) {                    // If gray, leave H & S at zero
+    if (delta != 0.0) {                    // If gray, leave H & S at zero
        HSV.y = delta / HSV.z;
        float3 delRGB;
        delRGB = (HSV.zzz - RGB + 3*delta) / (6.0*delta);
@@ -44,6 +55,12 @@ float3 rgb_to_hsv_no_clip(float3 RGB)
        else if ( RGB.b == HSV.z ) HSV.x = ( 2.0/3.0) + delRGB.g - delRGB.r;
     }
     return (HSV);
+}
+
+float3 adjust_hue(float3 HSV, float offset)
+{
+	if (HSV.y >= TOLERANCE) { HSV.x = fmod(HSV.x + offset, 1); }
+	return HSV;
 }
 
 float3 hsv_to_rgb(float3 HSV)
@@ -67,7 +84,7 @@ float4 main(float4 pos : SV_POSITION, float2 uv : TEXCOORD) : SV_TARGET
 {
  	float4 color = shaderTexture.Sample(samplerState, uv);
 	float3 hsv = rgb_to_hsv_no_clip(color.rgb);
-	hsv.x = fmod(hsv.r + HUE_OFFSET + Time * CHANGE_RATE, 1);
+	hsv = adjust_hue(hsv, HUE_OFFSET + Time * CHANGE_RATE);
 
     return float4(hsv_to_rgb(hsv), color.a);
 }
